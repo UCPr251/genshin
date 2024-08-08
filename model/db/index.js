beb5b6512d55c52acf57c10453b4b430fbcd9bc5
UCPr251 <2032385471@qq.com>
2024-08-09 02:09:19 +0800

    feat: 新增#yunzai-mys&&自动格式化源码

--- model/db/index.js
+++ model/db/index.js
@@ -1,6 +1,1 @@
-import UserDB from './UserDB.js'
-import MysUserDB from './MysUserDB.js'
-import UserGameDB from './UserGameDB.js'
-import { sequelize } from './BaseModel.js'
-
-export { UserDB, MysUserDB, UserGameDB, sequelize }
+ export { UserDB, MysUserDB, UserGameDB, sequelize } from "#yunzai-mys";

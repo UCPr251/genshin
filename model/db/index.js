64f92a35196b93b683a0f485c47329fcd27b2480
UCPr251 <2032385471@qq.com>
2024-08-09 21:12:39 +0800

    ???

--- model/db/index.js
+++ model/db/index.js
@@ -1,1 +1,6 @@
- export { UserDB, MysUserDB, UserGameDB, sequelize } from "#yunzai-mys";
+import UserDB from './UserDB.js'
+import MysUserDB from './MysUserDB.js'
+import UserGameDB from './UserGameDB.js'
+import { sequelize } from './BaseModel.js'
+
+export { UserDB, MysUserDB, UserGameDB, sequelize }
